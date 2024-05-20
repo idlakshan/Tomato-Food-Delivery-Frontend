7c9583cb2a1a235a66c0b2179e7b0d764f83910a
Dimuthu Lakshan <dimuthu.l@intelleon.com>
2024-05-20 20:41:34 +0530

    feat(Home): Add FoodDisplay component

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from "react";
+import {food_list} from '../assets/assets'
+
+export const StoreContext=createContext(null)
+
+const StoreContextProvider=(props)=>{
+
+    const [cartItems,setCartItems]=useState({});
+
+    const addToCart=(itemId)=>{
+     if(!cartItems[itemId]){
+       setCartItems((pre)=>({...pre,[itemId]:1}))
+     }else{
+        setCartItems((pre)=>({...pre,[itemId]:pre[itemId]+1}))
+     }
+    }
+
+    const removeFromCart=(itemId)=>{
+        setCartItems((pre)=>({...pre,[itemId]:pre[itemId]-1}))
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
+
+    const contextValue={
+        food_list,
+        cartItems,setCartItems,addToCart,removeFromCart
+    }
+
+    return(
+        <StoreContext.Provider value={contextValue}>
+                 {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider
